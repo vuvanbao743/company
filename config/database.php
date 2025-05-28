6310b7f0d7b68ba0b244bae77b8f0f4d14436a3b
baovu <vuvanbao347@gmail.com>
2025-05-29 06:42:35 +0700

    connect success to mongodb

--- config/database.php
+++ config/database.php
@@ -34,7 +34,7 @@ return [
         'mongodb' => [
             'driver' => 'mongodb',
             'dsn' => env('DB_URI'),
-            'database' => 'admin',
+            'database' => env('DB_DATABASE', 'admin'),
         ],
 
 
