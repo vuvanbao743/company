dad1ba8e24b5343515a39458ad607b00a6ff007f
baovu <vuvanbao347@gmail.com>
2025-05-29 06:24:43 +0700

    change env database in database.php

--- config/database.php
+++ config/database.php
@@ -34,7 +34,7 @@ return [
         'mongodb' => [
             'driver' => 'mongodb',
             'dsn' => env('DB_URI'),
-            'database' => env('DB_DATABASE', 'admindb'),
+            'database' => 'admin',
         ],
 
 
