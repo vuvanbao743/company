693f9a2316d9240c9c52ac9d3c89d15ed0dd06f8
baovu <vuvanbao347@gmail.com>
2025-05-29 17:05:32 +0700

    fix clound 1

--- config/cloudinary.php
+++ config/cloudinary.php
@@ -1,19 +1,12 @@
 <?php
 
 return [
-
     'cloud' => [
         'cloud_name' => env('CLOUDINARY_CLOUD_NAME'),
         'api_key'    => env('CLOUDINARY_API_KEY'),
         'api_secret' => env('CLOUDINARY_API_SECRET'),
     ],
-
     'url' => [
-        'secure' => true
+        'secure' => env('CLOUDINARY_SECURE', true),
     ],
-
-    'upload_preset'      => env('CLOUDINARY_UPLOAD_PRESET'),
-    'notification_url'   => env('CLOUDINARY_NOTIFICATION_URL'),
-    'upload_route'       => env('CLOUDINARY_UPLOAD_ROUTE'),
-    'upload_action'      => env('CLOUDINARY_UPLOAD_ACTION'),
 ];
