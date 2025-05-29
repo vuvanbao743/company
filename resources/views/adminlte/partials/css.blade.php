6a082da95be98dbc60867ab1d3d7c0fd3b8ffcda
baovu <vuvanbao347@gmail.com>
2025-05-29 08:42:27 +0700

    change local to cdn css js

--- resources/views/adminlte/partials/css.blade.php
+++ resources/views/adminlte/partials/css.blade.php
@@ -8,15 +8,16 @@
      integrity="sha256-tZHrRjVqNSRyWg2wbppGnT833E/Ys0DHWGwT04GiqQg=" crossorigin="anonymous" /> --}}
  <!--end::Third Party Plugin(OverlayScrollbars)-->
  <!--begin::Third Party Plugin(Bootstrap Icons)-->
- {{-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css"
-     integrity="sha256-9kPW/n5nn53j4WMRYAxe9c1rCY96Oogo/MKSVdKzPmI=" crossorigin="anonymous" /> --}}
+ <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css"
+     integrity="sha256-9kPW/n5nn53j4WMRYAxe9c1rCY96Oogo/MKSVdKzPmI=" crossorigin="anonymous" />
  <!--end::Third Party Plugin(Bootstrap Icons)-->
  <!--begin::Required Plugin(AdminLTE)-->
  <link rel="stylesheet" href="{{asset('assets/adminlte/dist/css/adminlte.css')}}" />
  <!--end::Required Plugin(AdminLTE)-->
  <!-- apexcharts -->
- <link href="css/bootstrap.min.css" rel="stylesheet" />
-<link rel="stylesheet" href="{{asset('assets/icon/icons/bootstrap-icons.css')}}">
+ {{-- <link href="css/bootstrap.min.css" rel="stylesheet" /> --}}
+ <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
+{{-- <link rel="stylesheet" href="{{asset('assets/icon/icons/bootstrap-icons.css')}}"> --}}
 
 
 
