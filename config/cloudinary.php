df3291a5776c3f26dacd8ea2bb5fda76b9b5bcc3
baovu <vuvanbao347@gmail.com>
2025-05-29 17:17:19 +0700

    add cloud_url

--- config/cloudinary.php
+++ config/cloudinary.php
@@ -1,6 +1,8 @@
 <?php
 
 return [
+    'cloud_url' => env('CLOUDINARY_URL'),
+
     'cloud' => [
         'cloud_name' => env('CLOUDINARY_CLOUD_NAME'),
         'api_key'    => env('CLOUDINARY_API_KEY'),
