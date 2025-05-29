c766b4324d66084aad525dd0deb13e836b2b9418
baovu <vuvanbao347@gmail.com>
2025-05-29 17:52:44 +0700

    add cloudinary

---
+++ config/cloudinary.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'cloud' => [
+        'cloud_name' => env('CLOUDINARY_CLOUD_NAME'),
+        'api_key'    => env('CLOUDINARY_API_KEY'),
+        'api_secret' => env('CLOUDINARY_API_SECRET'),
+    ],
+    'url' => [
+        'secure' => env('CLOUDINARY_SECURE', true),
+    ],
+];
